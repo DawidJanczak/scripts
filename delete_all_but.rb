12e20343688ae72fdaa1645545f483761023c6dd
Dawid Janczak <dawid.janczak@gmail.com>
2012-05-24 11:19:08 +0200

    Changed shebangs to point to correct ruby interpreter. rm_swap now accepts mandatory argument pointing to directory where the clean needs to be done. (cherry picked from commit 78dc58b582d3579617cef87040db16b957a94878)

--- delete_all_but.rb
+++ delete_all_but.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 to_remove = ARGV.map { |file| file.end_with?("/") ? file[0...-1] : file }
 Dir['*'].each { |file| `rm -rf #{file}` unless to_remove.include?(file) }
