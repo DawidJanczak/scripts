b694d5b05080fc697911caf16a21c7fa4947d635
Dawid Janczak <dawid.janczak@gmail.com>
2013-12-08 14:39:48 +0800

    Remove rm_swap script
    Custom swp location can be set in Vim and cleaned with a single rm

--- rm_swap.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-if (ARGV.size < 1)
-  $stderr.puts "Usage: ./rm_swap.rb <path_to_directory>"
-  exit 1
-end
-
-Dir.glob(File.join(ARGV[0], "**", ".*\.sw*")).each { |f| puts "Deleting #{f}"; File.delete(f) }
