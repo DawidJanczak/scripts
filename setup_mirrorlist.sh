53ea3f6e9bba769c7da6d2007244826af052b425
Dawid Janczak <dawid.janczak@gmail.com>
2016-04-25 21:44:18 +0800

    Add script to set up mirrorlist file

---
+++ setup_mirrorlist.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+old=/etc/pacman.d/mirrorlist
+new=$old.pacnew
+
+if [[ -f /etc/pacman.d/mirrorlist.pacnew ]]; then
+  mv $new $old
+  sed -i '/Taiwan/,/^$/{s/^#S/S/}' $old
+fi
