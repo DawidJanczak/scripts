5cb96bccc626c78ed99e1c275810bc4c3eaccaa8
Dawid Janczak <dawid.janczak@gmail.com>
2018-03-21 16:06:01 +0800

    Add magnet link transfer file opening magnet links with transmission

---
+++ magnet_link_transfer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+test -z $1 && echo "need magnet link!" && exit -1
+
+# the file should define HOST, PORT, USER and PASS variables
+source ./transmission_config
+
+LINK=$1
+PAUSED="false"
+
+SESSID=$(curl --silent --anyauth --user $USER:$PASS "http://$HOST:$PORT/transmission/rpc" | sed 's/.*<code>//g;s/<\/code>.*//g')
+
+curl --silent --anyauth --user $USER:$PASS \
+  --header "$SESSID" \
+  "http://$HOST:$PORT/transmission/rpc/" \
+  -d "{\"method\":\"torrent-add\",\"arguments\":{\"paused\":${PAUSED},\"filename\":\"${LINK}\"}}"
